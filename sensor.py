14583cd0d538dfbf7cf5dcfe7b24fa95b301fd5c
Aditya  Rola <adityarola@Adityas-MacBook-Air.local>
2018-04-06 21:42:02 +0530

    data check6

--- sensor.py
+++ sensor.py
@@ -1,9 +1,6 @@
-
 #Libraries
 import RPi.GPIO as GPIO
 import time
-import requests 
-import json
  
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
@@ -46,15 +43,8 @@ def distance():
 if __name__ == '__main__':
     try:
         while True:
-            print("script started")
             dist = distance()
-            print("distance DONE")
-            print(distance)
-            empty = 'true'
-            if (dist < 10):
-            	empty = 'false'
-            data={'space': dist , 'isEmpty' : empty}
-			# r = requests.put('http://finalyearproject.southindia.cloudapp.azure.com:8080/api/parking/58d388afa6e3d7355ce81aa9',data=data)
+            print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
  
         # Reset by pressing CTRL + C
